8d5a9116e6686830457524f0dddbfb53af86f571
tunglinn <tunglin@yahoo.com>
2023-05-17 23:14:58 -0700

    added render_keypoints

---
+++ render_keypoints.py
@@ -0,0 +1,26 @@
+from PIL import Image
+from PIL import ImageDraw
+from bs4 import BeautifulSoup
+
+img = Image.open(r'data\PascalVOC\TrainVal\VOCdevkit\VOC2011\JPEGImages\2008_000054.jpg')
+with open(r'data\PascalVOC\annotations\bird\2008_000054_2.xml') as f:
+    data = f.read()
+
+soup_data = BeautifulSoup(data, "xml")
+keypoints = soup_data.find_all('keypoint')
+
+print(keypoints)
+
+labels = []
+for keypoint in keypoints:
+    labels.append((keypoint['name'], int(float(keypoint['x'])), int(float(keypoint['y'])), int(float(keypoint['visible']))))
+
+draw = ImageDraw.Draw(img)
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'grey', 'black', 'white']
+circle_size = 3
+for label, color in zip(labels, colors):
+    draw.ellipse((label[1], label[2], label[1]+circle_size, label[2]+circle_size), width=5, fill=color)
+    draw.text((label[1]+40, label[2]), label[0], fill=color)
+
+img.show()
